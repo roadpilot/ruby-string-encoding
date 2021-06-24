d69bb519be3a3f7dec1215375f776aed1e608cbc
MartinAchterman <65299501+roadpilot@users.noreply.github.com>
2021-06-24 03:09:55 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,7 @@
+def transcode(input)
+    # my_string = input # create a string from a literal
+    # my_empty_string = String.new # create an empty string
+    # my_copied_string = String.new(my_string) # copy a string to a new variable
+    input.force_encoding('utf-8')
+    # print("My String's encoding: ", input.encoding.name) 
+end
